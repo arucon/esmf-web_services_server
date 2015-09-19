120c5f94e666c563839eea6125ddb599585ded32
rocky <rocky.dunlap@noaa.gov>
2015-09-18 21:51:48 -0600

    Python client readme

--- pythonclient/SimpleClient.py
+++ pythonclient/SimpleClient.py
@@ -1,20 +1,3 @@
-#
-# This requires pysimplesoap.
-#
-# https://github.com/pysimplesoap/pysimplesoap/releases
-# sudo python setup.py install	
-#
-#
-# If not installed, you'll need setuptools as well before installing above.
-#
-# https://pypi.python.org/pypi/setuptools
-# Download ez_setup.py and run "python ez_setup.py"
-#
-#
-# pysimplesoap:
-# https://github.com/pysimplesoap/pysimplesoap/releases
-# sudo python setup.py install
-
 from pysimplesoap.client import SoapClient
 
 #client = SoapClient(wsdl='http://localhost:8080/axis2/services/ESMFWebServicesService?wsdl',trace=False)
