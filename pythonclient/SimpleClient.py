20114bf9b950c9b5fdaf439e10acefd2e5c39869
rocky <rocky.dunlap@noaa.gov>
2015-09-20 15:19:20 -0600

    Updated python client

--- pythonclient/SimpleClient.py
+++ pythonclient/SimpleClient.py
@@ -1,19 +1,7 @@
 from pysimplesoap.client import SoapClient
 
-#client = SoapClient(wsdl='http://localhost:8080/axis2/services/ESMFWebServicesService?wsdl',trace=False)
-
-#response = client.NewClient(ServerName='localhost')
-#newClientId = response['ClientId']
-#print "new ClientId = ", newClientId
-
-#response = client.ComponentStatus(ServerName='localhost', ClientId=newClientId)
-#print response
-
-client = SoapClient(wsdl='http://localhost:8080/axis2/services/ESMF?wsdl',trace=True)
-response = client.NewModelService(app="app", compset="compset2")
-print response
-
-response = client.ListModelServices(dummy="1")
+client = SoapClient(wsdl='http://localhost:8080/axis2/services/ESMFWebServices?wsdl',trace=True)
+response = client.NewClient(ServerName="localhost")
 print response
 
 
