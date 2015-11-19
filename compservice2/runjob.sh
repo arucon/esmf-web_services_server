48953d90cdb3069d804c45481fd162e0d6f82ba2
rocky <rocky.dunlap@noaa.gov>
2015-11-19 13:46:53 -0700

    Removed binary files

--- compservice2/runjob.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Starting coupled flow component service"
-cd /home/yun/esmf-web_services_server2/compservice
-mpirun -np 4 ./ESMF_CoupledFlow
-echo "End of coupled flow component service"
