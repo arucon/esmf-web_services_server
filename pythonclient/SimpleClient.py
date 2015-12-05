5f64112b026da456ca929f17cd7b415ee039479e
rocky <rocky.dunlap@noaa.gov>
2015-12-04 17:40:26 -0700

    Modified python client

--- pythonclient/SimpleClient.py
+++ pythonclient/SimpleClient.py
@@ -1,7 +1,8 @@
 from pysimplesoap.client import SoapClient
 import time
 
-client = SoapClient(wsdl='http://localhost:8080/axis2/services/ESMFWebServices?wsdl',trace=False)
+#client = SoapClient(wsdl='http://localhost:8080/axis2/services/ESMFWebServices?wsdl',trace=False)
+client = SoapClient(wsdl='http://52.21.215.244:8080/axis2/services/ESMFWebServices?wsdl',trace=False)
 
 response = client.NewClient(ServerName="localhost") 
 print response
@@ -11,32 +12,38 @@ ID = response['ClientId']
 print "Sleeping for 5 seconds...."
 time.sleep(5)
 
+print "Initializing component..."
 response = client.InitComponent(ServerName="localhost", ClientId=ID)
 print response
 
+#for i in range(10):
+#    print "Updating status..."
+#    response = client.ComponentStatus(ServerName="localhost", ClientId=ID)
+#    print response
+#    time.sleep(1)
 
 print "Sleeping for 5 seconds...."
 time.sleep(5)
 
+print "Running component..."
 response = client.RunComponent(ServerName="localhost", ClientId=ID)
 print response
-
 
 print "Sleeping for 10 seconds...."
 time.sleep(10)
 
+print "Finalizing component..."
 response = client.FinalizeComponent(ServerName="localhost", ClientId=ID)
 print response
 
 print "Sleeping for 5 seconds...."
 time.sleep(5)
 
+print "Ending session..."
 response = client.EndClient(ServerName="localhost", ClientId=ID)
 print response
 
 
-#response = client.SetInputData(ServerName="localhost",ClientId = ID)
-#response = client.GetOutputData(ServerName="localhost",ClientId = ID)
 
 
 
